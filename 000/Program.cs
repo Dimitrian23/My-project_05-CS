f61cb016bb78503bf6b5f297647ae478e02d1422
Dmitrian23 <dimonio1991@mail.ru>
2023-02-15 22:45:39 +0300

    Задача 000

---
+++ 000/Program.cs
@@ -0,0 +1,61 @@
+﻿// Переворот массива
+
+
+// int[]a={3,2,7,1,9};
+//            System.Console.Write("Вариант (А) оригинальный:");
+//    for(int i=0;i<a.Length;i++)
+//       {
+//            System.Console.Write($" {a[i]} ");
+//       }
+//            System.Console.WriteLine();
+// int t;
+//    for(int i=0;i<a.Length/2;i++)
+//       {
+//         t=a[i];
+//         a[i]=a[a.Length-i-1];
+//         a[a.Length-i-1]=t;
+//       }
+//            System.Console.Write("Вариант (В) перевёрнутый:");
+//    for(int i=0;i<a.Length;i++)
+//       {
+//            System.Console.Write($" {a[i]} ");
+//       }
+
+
+
+int[]a={3,2,7,1,9};
+           System.Console.Write("Вариант (А) оригинальный:");
+   for(int i=0;i<a.Length;i++)
+      {
+           System.Console.Write($" {a[i]} ");
+      }
+           System.Console.WriteLine();
+
+  ReverseArray(a);
+  
+            System.Console.Write("Вариант (В) перевёрнутый:");
+
+   for(int i=0;i<a.Length;i++)
+      {
+           System.Console.Write($" {a[i]} ");
+      }
+
+void Swap(ref int a, ref int b)
+      {
+         int t=a;
+         a=b;
+         b=t;
+      }
+
+void ReverseArray(int[] a)
+{
+    for(int i=0;i<a.Length/2;i++)
+      {
+       Swap(ref a[i], ref a[a.Length-i-1]);
+      }
+}
+
+
+
+
+
