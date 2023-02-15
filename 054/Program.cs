5f4b3a8fbdc2634d1168291243b5d68b8be62d14
Dmitrian23 <dimonio1991@mail.ru>
2023-02-15 22:47:06 +0300

    Задача 054

---
+++ 054/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача_№54 С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+System.Console.WriteLine("введите число N");
+int N=Convert.ToInt32(Console.ReadLine());
+int [] fib=new int[N];
+int a0=0;
+int a1=1;
+
+fib[0]=a0;
+fib[1]=a1;
+for(int i=2;i<N;i++)
+{
+    int a=a0+a1;
+    fib[i]=a;
+    a0=a1;
+    a1=a;    
+}
+for(int i=0;i<fib.Length;i++)
+{
+     System.Console.Write($"_{fib[i]}_");
+}
+
+
+
+
+
+
+
