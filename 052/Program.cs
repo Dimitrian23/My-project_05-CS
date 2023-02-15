54add0b4021118c7ee808086ac4e439251134b7b
Dmitrian23 <dimonio1991@mail.ru>
2023-02-15 22:46:38 +0300

    Задача 052

---
+++ 052/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача_№52 Написать программу преобразования десятичного числа в двоичное
+
+System.Console.WriteLine("Введите число:");
+        int N=Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine();
+int S=N;        
+int N2=0,k=0;
+while(N!=0)
+{
+    N2=N2+N%2*(int)Math.Pow(10,k);
+    N=N/2;
+    k++;
+}
+System.Console.WriteLine($"Преобразую десятичное число {S} в двоичное {N2} ");
+
