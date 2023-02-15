512a864f55d221fce0b8f9521295db5694636e38
Dmitrian23 <dimonio1991@mail.ru>
2023-02-15 22:46:04 +0300

    Задача 050

---
+++ 050/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача_№50 Выяснить являются ли три числа сторонами треугольника.
+
+int a,b,c;
+       System.Console.WriteLine("Введите последовательно три числа подтверждая ввод каждого нажатием клавиши Enter");
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            c=Convert.ToInt32(Console.ReadLine());
+
+   if(a+b>c && b+c>a && a+c>b) 
+     {
+       System.Console.WriteLine("Являются");
+     }
+    else 
+     {
+        System.Console.WriteLine("Не являются");
+     } 
+
