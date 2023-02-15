d2f62bb5fc22ec66122a2833f67d614b9290ceb5
Dmitrian23 <dimonio1991@mail.ru>
2023-02-15 22:47:40 +0300

    Задача 056

---
+++ 056/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача_№56 Написать программу копирования массива
+
+ int N;
+    System.Console.Write("Задайте желаемую длину массива:");
+         N=Convert.ToInt32(Console.ReadLine());
+
+int [] M;
+M=new int[N]; 
+
+Random random=new Random();
+  for(int i=0;i<M.Length;i++)
+    M[i]=random.Next(1,999);
+
+    System.Console.Write("Дан массив случайных чисел:--->");
+   for(int i=0;i<M.Length;i++)
+    System.Console.Write($" [ {M[i]} ] ");
+    System.Console.WriteLine();
+int [] M1;
+M1=new int[M.Length]; 
+Array.Copy(M,M1,M.Length);
+System.Console.Write("Копия массива----------------->");
+ for(int i=0;i<M1.Length;i++)
+  System.Console.Write($" [ {M1[i]} ] ");
+
+
+
